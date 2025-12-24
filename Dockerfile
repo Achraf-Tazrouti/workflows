b15a48672f683b983562fb5a89cb38374b4d2c3d
Achraf Tazrouti <s161474@ap.be>
2025-12-24 04:10:37 +0100

    manual works automatic not

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM n8nio/n8n
+
+USER root
+RUN apk add --no-cache bash
+USER node
--- import-workflows.sh
+++ import-workflows.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-
-echo "Importing workflows..."
-
-n8n import:workflow --input=/workflows --separate --force
-
-echo "Done."
+#!/bin/bash
+set -e
+echo "== Importeren van workflows =="
+n8n import:workflow --input=/workflows/achraf_email_generator_chatbot.json || true
+n8n import:workflow --input=/workflows/Achraf_template_checker_chat.json || true
+n8n import:workflow --input=/workflows/chatbot_SenChraf.json || true
+echo "== Importeren klaar =="
